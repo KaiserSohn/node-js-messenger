e725660028dfcf102ebb5d18454a7ed76dab9810
OsterHaseMops <osterhasemops@gmail.com>
2022-07-26 12:16:15 +0300

    Исправления docker build

--- app/components/storage/historyCleanerService.js
+++ app/components/storage/historyCleanerService.js
@@ -7,7 +7,8 @@ const FILESIZE_FOR_DELETE_OLD_MESSAGE = 1;
 module.exports = async () => {
     await fileSystem.stat(MessageStorageService.filename, (err, stats) => {
         if (err) {
-            throw new Error('Произошла ошибка при попытке удалить старые сообщения');
+            console.log('Произошла ошибка при попытке удалить старые сообщения')
+            return;
         }
         let size = stats.size / (1024 * 1024);
 
